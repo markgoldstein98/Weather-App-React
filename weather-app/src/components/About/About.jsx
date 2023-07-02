c909ac0ddd765c14d956ed7b0f43500e69b309ad
Mark Abraham Goldstein <123721646+markgoldstein98@users.noreply.github.com>
2023-07-02 15:01:44 +0200

    created components

---
+++ weather-app/src/components/About/About.jsx
@@ -0,0 +1,39 @@
+
+function aboutPage() {
+    
+    return(
+        <section>
+            <h1> Welcome to the Weather App!</h1>
+            <h3>
+            This WeatherApp is a React application that fetches weather data from the WeatherAPI and displays it to the user. It provides current weather information for a specified location, allows searching for cities, sets the background image based on the selected city, and enables saving favorite cities.
+
+Features:
+
+Current weather information: Display the current temperature, weather condition, humidity, and wind speed for the specified location.
+City search: Enter a city name in the input field and click the "Search" button to retrieve weather information for that city.
+Background image: Set a background image based on the selected city to enhance the visual experience.
+Save favorite cities: Save frequently visited cities to easily access their weather information later.
+Usage:
+
+Open the WeatherApp in your web browser.
+
+Enter a city name in the search bar and click the "Search" button.
+
+The current weather information for the specified city will be displayed, along with the corresponding background image.
+
+To save the city as a favorite, click the "Save as Favorite" button. The city will be added to your favorites list.
+
+To view the weather for a favorite city, click on the city name in the favorites list. The weather information for that city will be displayed.
+
+Technologies Used:
+
+React: The main JavaScript library used for building the application, handling state management, and rendering components.
+HTML: The structure and layout of the application.
+CSS: Styling and presentation of the user interface.
+            </h3>
+        </section>
+
+    )
+}
+
+export default aboutPage
--- weather-app/src/components/navBar/Navbar.jsx
+++ weather-app/src/components/navBar/Navbar.jsx
@@ -1,12 +1,27 @@
 import "./navBar.css";
+import aboutPage from "../About/About";
 function HeaderNav(){
 
+
     return (
+
+        function onClickAbout() {
+          // kattintásra a aboutPage jelenjen meg, illetve landing page-kent is
+        },
+
+        function onClickSeach() {
+          // kattintásra bejön a Search Weather felület  
+        },
+
+        function onClickSupport() {
+            // kattintásra bejön a Support
+        },
+
         <nav className="navbar">
             <section className="navigation">
-                <a href="">About</a>
-                <a href="">Search Weather</a>
-                <a href="">Support</a>
+                <a href="/" onClick={aboutPage}>About</a>
+                <a href="/">Search Weather</a>
+                <a href="/">Support</a>
             </section>
          </nav>
 
