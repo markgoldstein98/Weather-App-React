6b4ff79785b4677cefd3646b0118d4f9ba57a473
Mark Abraham Goldstein <123721646+markgoldstein98@users.noreply.github.com>
2023-07-02 13:56:18 +0200

    added nav bar

---
+++ weather-app/src/components/navBar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./navBar.css";
+function HeaderNav(){
+
+    return (
+        <nav className="navbar">
+            <section className="navigation">
+                <a href="">About</a>
+                <a href="">Search Weather</a>
+                <a href="">Support</a>
+            </section>
+         </nav>
+
+    )
+}
+
+export default HeaderNav;
