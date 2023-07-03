3118dd43172bdb4e0fbdea2e9a9e64acfbcc41f2
Mark Abraham Goldstein <123721646+markgoldstein98@users.noreply.github.com>
2023-07-03 18:36:52 +0200

    usestate

--- weather-app/src/components/InputField/SearchWeather.jsx
+++ weather-app/src/components/InputField/SearchWeather.jsx
@@ -1,9 +1,34 @@
+import { useEffect } from "react";
+import "./searchweather.css";
+
 function Weather() {
+    const API_KEY = "8be1860db359470181e104408231505";
+    let API_CITY = "Budapest";
+
+    useEffect(()=> {
+        getFetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${API_CITY}`)
+
+    },[])
+
+    async function getFetch(url) {
+        const response = await fetch (url)
+        const data = await response.json()
+
+        return data
+    }
+        console.log(getFetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${API_CITY}`))
       
+
+        
+
+
     return(
-        <div>
-            Search here!
-        </div>
+        <section className="container">
+            <form >
+                <input type="text" />
+                <button>Search</button>
+            </form>
+        </section>
     )
 }
 
